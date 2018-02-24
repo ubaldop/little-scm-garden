4742454b858e15a8fa85c4311f3b7d4a63a25152
Ubaldo Pescatore <ubaldopescatore@gmail.com>
2018-02-24 12:49:11 +0100

    functions about sets done

--- chapter7.scm
+++ chapter7.scm
@@ -38,5 +38,47 @@
                         (else (cons (car lat) (makesetR (multirember (car lat) (cdr lat)))))
                         )))
 
+
+(define subset
+    (lambda (set1 set2) 
+        (cond 
+            ((null? set1) #t)
+            ((member? (car set1) set2) (subset (cdr set1) set2))
+            (else #f))))
+
+ (define eqset?
+     (lambda (set1 set2) (
+         and (subset set1 set2) 
+            (subset set2 set1))))
+            
+(define insersect?
+     (lambda (set1 set2) 
+          (cond 
+                ((null? set1) #f)
+                ((or (member? (car set1) set2) (intersect? (cdr set1) set2))))))      
+                    
+(define insersect
+    (lambda (set1 set2) 
+        (cond 
+            ((null? set1) '())
+            ((member? (car set1) set2) (cons (car set1) (intersect (cdr set1) set2)))
+            (else (intersect (cdr set1) set2)))))  
+                
+                    
+ (define union
+        (lambda (set1 set2) 
+            (cond 
+                ((null? set1) set2)
+                ((member? (car set1) set2) (union (cdr set1) set2))
+                ((cons (car set1) (union (cdr set1) set2))))))
+
+                    
+(define intersectall
+    (lambda (lset) 
+        (cond 
+            ((null? (cdr lset)) (car lset))
+            ((intersect (car lset) (intersectall (cdr lset)))))))
+
+
 ;;; that's the example
 (define l '(apple peach pear peach plum apple lemon peach))                        
